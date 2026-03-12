4c19af936d132e0f7f204da9a5382440727d29c4
Bhargavi594 <bhargaviyennaboina@gmail.com>
2026-03-12 16:57:46 +0530

    Create train_model.py

---
+++ train_model.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import joblib
+
+data = pd.read_csv("data/transactions.csv")
+
+X = data.drop("is_fraud", axis=1)
+y = data["is_fraud"]
+
+X = pd.get_dummies(X)
+
+# Train test split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# Train model
+model = RandomForestClassifier(n_estimators=100)
+model.fit(X_train, y_train)
+
+# Save model
+joblib.dump(model, "model/fraud_model.pkl")
+
+print("Model trained and saved!")
